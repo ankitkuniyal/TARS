2f4951e78977cf5e8fd36e73dcec8040c511529e
Ankit Kuniyal <ankitkuniyal520@gmail.com>
2026-01-16 21:23:23 +0000

    Add Clerk authentication and update dependencies

--- tars/src/app/layout.tsx
+++ tars/src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 import "./globals.css";
 
 const inter = Inter({
@@ -27,6 +35,10 @@ export default function RootLayout({
 }>) {
    return (
     <>
+    <ClerkProvider 
+    appearance={{ 
+      theme :dark,
+      }}>
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
@@ -36,11 +48,25 @@ export default function RootLayout({
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-          >
+            >
             {children}
+             <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
           </ThemeProvider>
         </body>
       </html>
+      </ClerkProvider>
     </>
   );
 }
